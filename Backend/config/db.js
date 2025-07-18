7d69e98f1fe6250912ca9c710a7c8b60a4dde904
AbhishekMehandiya <abhishekmehandiya2@gmail.com>
2025-07-18 10:24:32 +0530

    Add Inngest functions for Clerk user sync

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -4,7 +4,7 @@ const connectDb=async()=>{
         mongoose.connection.on('connected',()=>console.log('Database connected'))
 await mongoose.connect(`${process.env.MONGO_URI}`);
     } catch(e){
-        console.log(e)
+        console.log(e);
     }
     
 }
--- Backend/inngest/index.js
+++ Backend/inngest/index.js
@@ -48,5 +48,5 @@ const syncUserUpdation= inngest.createFunction(
 // Create an empty array where we'll export future Inngest functions
 export const functions = [syncUserCreation,
     syncUserDeletion,
-    syncUserUpdation
+    syncUserUpdation,
 ];
